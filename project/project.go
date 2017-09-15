0ebabae51bc86a2ba245029cbfde8345a824f593
Ian Sowinski <sowinj@gmail.com>
2017-09-16 00:22:26 +0200

    initial commit

---
+++ project/project.go
@@ -0,0 +1,15 @@
+package project
+
+import (
+    "math/rand"
+    "time"
+    // "fmt"
+)
+
+//go:generate go run tweets.go
+
+func PrintRandomTweet() string{
+  rand.Seed(time.Now().Unix())
+  randomIndex := rand.Intn(maxItem)
+  return allTweets[randomIndex]
+}
---
+++ wisdom.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+    "./project"
+    "github.com/fatih/color"
+)
+
+func main() {
+  colored := color.New(color.FgWhite, color.Bold)
+  colored.Printf(project.PrintRandomTweet())
+  colored.Printf("\n")
+}
