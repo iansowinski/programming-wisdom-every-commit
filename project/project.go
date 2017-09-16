9406cfc9967dc4dea36ae94c021c1b341922a8e5
Ian Sowinski <sowinj@gmail.com>
2017-09-16 11:26:03 +0200

    go fmt

--- project/project.go
+++ project/project.go
@@ -1,15 +1,15 @@
 package project
 
 import (
-    "math/rand"
-    "time"
-    // "fmt"
+	"math/rand"
+	"time"
+	// "fmt"
 )
 
 //go:generate go run tweets.go
 
-func PrintRandomTweet() string{
-  rand.Seed(time.Now().Unix())
-  randomIndex := rand.Intn(maxItem)
-  return allTweets[randomIndex]
+func PrintRandomTweet() string {
+	rand.Seed(time.Now().Unix())
+	randomIndex := rand.Intn(maxItem)
+	return allTweets[randomIndex]
 }
--- wisdom.go
+++ wisdom.go
@@ -1,12 +1,12 @@
 package main
 
 import (
-    "./project"
-    "github.com/fatih/color"
+	"./project"
+	"github.com/fatih/color"
 )
 
 func main() {
-  colored := color.New(color.FgWhite, color.Bold)
-  colored.Printf(project.PrintRandomTweet())
-  colored.Printf("\n")
+	colored := color.New(color.FgWhite, color.Bold)
+	colored.Printf(project.PrintRandomTweet())
+	colored.Printf("\n")
 }
